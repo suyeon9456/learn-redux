eee7cf12aabb9799a1fcdae9ff997d916e64dc23
suyeon <suyeon9456@naver.com>
2021-04-29 18:49:34 +0900

    redux-thunk로 Promise 다루기 B - reducerUtils 작성

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,4 +1,5 @@
 import * as postsAPI from '../api/posts'
+import { reducerUtils } from '../lib/asyncUtils'
 
 const GET_POSTS = 'GET_POSTS'
 const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'
@@ -46,5 +47,48 @@ export const getPost = (id) => async dispatch => {
       type: GET_POST_ERROR,
       error: e
     })
+  }
+}
+
+const initialState = {
+  posts: reducerUtils.initial,
+  post: reducerUtils.initial
+}
+
+export default function posts (state = initialState, action) {
+  switch (action.type) {
+    case GET_POSTS:
+      return {
+        ...state,
+        // posts: reducerUtils.loading(state.posts.data) // 기존의 데이터 값을 유지하고 로딩값만 변경하고 싶을 떄
+        posts: reducerUtils.loading()
+      }
+    case GET_POSTS_SUCCESS:
+      return {
+        ...state,
+        posts: reducerUtils.success(action.posts)
+      }
+    case GET_POSTS_ERROR:
+      return {
+        ...state,
+        posts: reducerUtils.error(action.error)
+      }
+    case GET_POST:
+      return {
+        ...state,
+        post: reducerUtils.loading()
+      }
+    case GET_POST_SUCCESS:
+      return {
+        ...state,
+        post: reducerUtils.success(action.post)
+      }
+    case GET_POST_ERROR:
+      return {
+        ...state,
+        post: reducerUtils.error(action.error)
+      }
+    default:
+      return state
   }
 }
