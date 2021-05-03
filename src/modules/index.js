19eb0af4e0fc483e0c5dc3fb29af1b3de62896b7
suyeon <suyeon9456@naver.com>
2021-05-03 17:22:43 +0900

    redux-thunk로 Promise 다루기 D - hadleAsyncActions

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import counter from './counter'
+import posts from './posts'
 // import todos from './todos'
 
 // 루트 리듀서만들기
 const rootReducer = combineReducers({
-  counter
+  counter,
+  posts
 })
 
 export default rootReducer
--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,5 +1,5 @@
 import * as postsAPI from '../api/posts'
-import { createPromiseThunk, reducerUtils } from '../lib/asyncUtils'
+import { createPromiseThunk, hanldleAsyncActions, reducerUtils } from '../lib/asyncUtils'
 
 const GET_POSTS = 'GET_POSTS'
 const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'
@@ -15,43 +15,23 @@ export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts)
 export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById)
 
 const initialState = {
-  posts: reducerUtils.initial,
-  post: reducerUtils.initial
+  posts: reducerUtils.initial(),
+  post: reducerUtils.initial()
 }
+
+const getPostsReducer = hanldleAsyncActions(GET_POSTS, 'posts')
+const getPostReducer = hanldleAsyncActions(GET_POST, 'post')
 
 export default function posts (state = initialState, action) {
   switch (action.type) {
     case GET_POSTS:
-      return {
-        ...state,
-        // posts: reducerUtils.loading(state.posts.data) // 기존의 데이터 값을 유지하고 로딩값만 변경하고 싶을 떄
-        posts: reducerUtils.loading()
-      }
     case GET_POSTS_SUCCESS:
-      return {
-        ...state,
-        posts: reducerUtils.success(action.payload)
-      }
     case GET_POSTS_ERROR:
-      return {
-        ...state,
-        posts: reducerUtils.error(action.payload)
-      }
+      return getPostsReducer(state, action)
     case GET_POST:
-      return {
-        ...state,
-        post: reducerUtils.loading()
-      }
     case GET_POST_SUCCESS:
-      return {
-        ...state,
-        post: reducerUtils.success(action.payload)
-      }
     case GET_POST_ERROR:
-      return {
-        ...state,
-        post: reducerUtils.error(action.payload)
-      }
+      return getPostReducer(state, action)
     default:
       return state
   }
