aac190158379c0bbe38a0a95586991bdfd46e630
suyeon <suyeon9456@naver.com>
2021-05-03 17:45:52 +0900

    react-thunk로 Promise 다루기 E - 포스트 리스트 구현하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import CounterContainer from './containers/CounterContainer'
+// import CounterContainer from './containers/CounterContainer'
+import PostListContainer from './containers/PostListContainer'
 // import TodosContainer from './containers/TodosContainer'
 
 function App () {
   return (
     <div>
-      <CounterContainer />
+      <PostListContainer />
+      {/* <CounterContainer /> */}
       {/* <hr />
       <TodosContainer /> */}
     </div>
