653eeef88a0013d3ac2de010c7be958fcb50d1ca
suyeon <suyeon9456@naver.com>
2021-05-12 17:39:16 +0900

    json-server

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,30 +1,12 @@
-const sleep = n => new Promise(resolve => setTimeout(resolve, n))
-
-// { id, title, body }
-const posts = [
-  {
-    id: 1,
-    title: 'test1',
-    body: 'text content1'
-  },
-  {
-    id: 2,
-    title: 'test2',
-    body: 'text content2'
-  },
-  {
-    id: 3,
-    title: 'test3',
-    body: 'text content3'
-  }
-]
+import axios from 'axios'
+// npx json-server ./data.json --port 4000 서버 생성 명렁어
 
 export const getPosts = async () => {
-  await sleep(500)
-  return posts
+  const response = await axios.get('http://localhost:4000/posts')
+  return response.data
 }
 
 export const getPostById = async (id) => {
-  await sleep(500)
-  return posts.find(post => post.id === id)
+  const response = await axios.get(`http://localhost:4000/posts/${id}`)
+  return response.data
 }
