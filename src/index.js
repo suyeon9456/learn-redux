595ae9cb7073f9adb3eb011edfda83f181d8aa44
suyeon <suyeon9456@naver.com>
2021-05-13 18:06:04 +0900

    redux-saga E - 라우터 연동

--- src/index.js
+++ src/index.js
@@ -15,7 +15,11 @@ import { createBrowserHistory } from 'history'
 import createSagaMiddleware from 'redux-saga'
 
 const customHistory = createBrowserHistory()
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware({
+  context: {
+    history: customHistory
+  }
+})
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware, ReduxThunk.withExtraArgument({ history: customHistory }), logger)))
 // const store = createStore(rootReducer, composeWithDevTools())
--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,6 +1,6 @@
 import * as postsAPI from '../api/posts'
 import { hanldleAsyncActions, hanldleAsyncActionsById, reducerUtils, createPromiseSaga, createPromiseSagaById } from '../lib/asyncUtils'
-import { takeEvery } from 'redux-saga/effects'
+import { takeEvery, getContext } from 'redux-saga/effects'
 
 const GET_POSTS = 'GET_POSTS'
 const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'
@@ -10,6 +10,7 @@ const GET_POST = 'GET_POST'
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS'
 const GET_POST_ERROR = 'GET_POST_ERROR'
 
+const GO_TO_HOME = 'GO_TO_HOME'
 const CLEAR_POST = 'CLEAR_POST'
 
 // thunk 호출함수
@@ -22,16 +23,19 @@ export const getPost = id => ({ type: GET_POST, payload: id, meta: id })
 const getPostsSaga = createPromiseSaga(GET_POSTS, postsAPI.getPosts)
 
 const getPostSaga = createPromiseSagaById(GET_POST, postsAPI.getPostById)
+function * goToHomeSaga () {
+  const history = yield getContext('history')
+  history.push('/')
+}
 
 export function * postSaga () {
   yield takeEvery(GET_POSTS, getPostsSaga)
   yield takeEvery(GET_POST, getPostSaga)
+  yield takeEvery(GO_TO_HOME, goToHomeSaga)
 }
 
 export const clearPost = () => ({ type: CLEAR_POST })
-export const goHome = () => (dispatch, getState, { history }) => {
-  history.push('/')
-}
+export const goHome = () => ({ type: GO_TO_HOME })
 
 const initialState = {
   posts: reducerUtils.initial(),
