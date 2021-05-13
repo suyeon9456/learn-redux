ff32d3d433c2d196fbdcb09d5cda63cc99f3edcf
suyeon <suyeon9456@naver.com>
2021-05-13 17:26:10 +0900

    redux-saga C - 프로미스 다루기

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
 import counter, { counterSaga } from './counter'
-import posts from './posts'
+import posts, { postSaga } from './posts'
 // import todos from './todos'
 import { all } from 'redux-saga/effects'
 
@@ -11,7 +11,7 @@ const rootReducer = combineReducers({
 })
 
 export function * rootSaga () {
-  yield all([counterSaga()])
+  yield all([counterSaga(), postSaga()])
 }
 
 export default rootReducer
